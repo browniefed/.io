9a283becda4f25484cb1c72842cb924a8fd5c6f4
Jason Brown <browniefed@gmail.com>
2014-11-29 13:26:25 -0800

    Add in base jama.io stuff

---
+++ js/jama.js
@@ -0,0 +1,17 @@
+// (function() {
+//     var navClass = 'jama-nav';
+//     document.addEventListener( 'click', function ( e ) {
+//         var isNav = e.target.classList.contains(navClass),
+//             contentClass,
+//             visible;
+//         if (isNav) {
+//             contentClass = e.target.className.replace(navClass, '').trim();
+//             visible = document.querySelector('.visible');
+//             if (visible) {
+//                 visible.classList.remove('visible');
+//                 document.querySelector('.content .' + contentClass).classList.add('visible');
+//             }
+          
+//         }
+//     }, false );
+// }())
